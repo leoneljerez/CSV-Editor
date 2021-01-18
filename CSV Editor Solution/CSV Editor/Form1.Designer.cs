6b8d6631fcd56c5b0c1f03b63ede99acb536d88b
leoneljerez <31394436+leoneljerez@users.noreply.github.com>
2021-01-18 16:19:07 -0500

    initial

---
+++ CSV Editor Solution/CSV Editor/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace CSV_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnInput = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.radioSmaller = new System.Windows.Forms.RadioButton();
+            this.radioHalf = new System.Windows.Forms.RadioButton();
+            this.btnOutput = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(503, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnInput
+            // 
+            this.btnInput.Location = new System.Drawing.Point(598, 32);
+            this.btnInput.Name = "btnInput";
+            this.btnInput.Size = new System.Drawing.Size(75, 23);
+            this.btnInput.TabIndex = 2;
+            this.btnInput.Text = "...";
+            this.btnInput.UseVisualStyleBackColor = true;
+            this.btnInput.Click += new System.EventHandler(this.btnInput_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnInput);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(720, 83);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input File";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input Path:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.btnConvert);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.groupBox3);
+            this.groupBox2.Controls.Add(this.btnOutput);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 101);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(720, 144);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output File";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(563, 68);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(110, 41);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output Path:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.radioSmaller);
+            this.groupBox3.Controls.Add(this.radioHalf);
+            this.groupBox3.Location = new System.Drawing.Point(22, 60);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(174, 73);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Changes";
+            // 
+            // radioSmaller
+            // 
+            this.radioSmaller.AutoSize = true;
+            this.radioSmaller.Location = new System.Drawing.Point(20, 44);
+            this.radioSmaller.Name = "radioSmaller";
+            this.radioSmaller.Size = new System.Drawing.Size(123, 17);
+            this.radioSmaller.TabIndex = 1;
+            this.radioSmaller.TabStop = true;
+            this.radioSmaller.Text = "Smaller than 100 MB";
+            this.radioSmaller.UseVisualStyleBackColor = true;
+            // 
+            // radioHalf
+            // 
+            this.radioHalf.AutoSize = true;
+            this.radioHalf.Location = new System.Drawing.Point(20, 20);
+            this.radioHalf.Name = "radioHalf";
+            this.radioHalf.Size = new System.Drawing.Size(85, 17);
+            this.radioHalf.TabIndex = 0;
+            this.radioHalf.TabStop = true;
+            this.radioHalf.Text = "Half the Size";
+            this.radioHalf.UseVisualStyleBackColor = true;
+            // 
+            // btnOutput
+            // 
+            this.btnOutput.Location = new System.Drawing.Point(598, 32);
+            this.btnOutput.Name = "btnOutput";
+            this.btnOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnOutput.TabIndex = 2;
+            this.btnOutput.Text = "...";
+            this.btnOutput.UseVisualStyleBackColor = true;
+            this.btnOutput.Click += new System.EventHandler(this.btnOutput_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(89, 34);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(503, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(563, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(110, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(742, 258);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "CSV Editor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnInput;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton radioSmaller;
+        private System.Windows.Forms.RadioButton radioHalf;
+        private System.Windows.Forms.Button btnOutput;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ CSV Editor Solution/CSV Editor/Form1.cs
@@ -0,0 +1,215 @@
+﻿/*
+ ***************************************************************************
+ *                                                                         *
+ *                              Leonel Jerez                               *
+ *                                9/5/2018                                 *
+ *                              CSV Editor v1                              *
+ *                                                                         *
+ ***************************************************************************
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_Editor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInput_Click(object sender, EventArgs e)
+        {
+            string fileLocation = null;
+
+            //Dialog Box to open file
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.Title = "Open a CSV File";
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.RestoreDirectory = true;
+                openFileDialog1.Multiselect = false;
+
+                //Check to make sure file is good.
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileLocation = openFileDialog1.FileName;
+                }
+            }
+
+            //Check to see if its empty
+            if (fileLocation != null)
+            {
+                textBox1.Text = fileLocation;            
+            }
+        }
+
+        private void btnOutput_Click(object sender, EventArgs e)
+        {
+            string fileLocation = null;
+
+            //Dialog Box to open file
+            using (SaveFileDialog openFileDialog1 = new SaveFileDialog())
+            {
+                openFileDialog1.Title = "Open a CSV File";
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "csv files (*.csv)|*.csv";
+                openFileDialog1.FilterIndex = 1;
+                openFileDialog1.RestoreDirectory = true;
+
+                //Check to make sure file is good.
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    fileLocation = openFileDialog1.FileName;
+                }
+            }
+
+            //Check to see if its empty
+            if (fileLocation != null)
+            {
+                textBox2.Text = fileLocation;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            //Gets input and output locations
+            string openLocation = textBox1.Text;
+            string saveLocation = textBox2.Text;
+            string[] data;
+
+
+            using (StreamReader sr = new StreamReader(openLocation))
+            {
+
+                /*
+                 ***************************************************************************
+                 *                                                                         *
+                 *                  Check for half the size radio button                   *
+                 *                                                                         *
+                 ***************************************************************************
+                 */
+                if (radioHalf.Checked)
+                {
+                    int rows = 0;
+
+                    //Count number of rows in csv file
+                    using (var reader = File.OpenText(openLocation))
+                    {
+                        while (reader.ReadLine() != null)
+                        {
+                            rows++;
+                        }
+                    }
+
+                    //divide by two for half
+                    rows = rows / 2;
+                    //creates new array using the amount of rows
+                    data = new string[rows];
+
+
+                    //Populate array with CSV rows
+                    int arrayInjection = 0;
+                    using (var reader = File.OpenText(openLocation))
+                    {
+                        while (reader.ReadLine() != null && arrayInjection != rows)
+                        {
+                            data[arrayInjection] = reader.ReadLine();
+                            arrayInjection++;
+                        }
+                    }
+
+                    if (!File.Exists(saveLocation))
+                    {
+                        File.Create(saveLocation).Close();
+                    }
+
+                    //Starts process for saving the file into a new document
+                    StringBuilder sb = new StringBuilder();
+                    for (int row = 0; row < rows; row++)
+                    {
+                        sb.Append(data[row]);
+                        sb.AppendLine();
+                    }
+
+                    File.WriteAllText(saveLocation, sb.ToString());
+                    textBox3.Text = "Completed";
+                }
+
+                /*
+                 ***************************************************************************
+                 *                                                                         *
+                 *             Check for smaller than 100MB  radio button                  *
+                 *                                                                         *
+                 ***************************************************************************
+                 */
+                else if (radioSmaller.Checked)
+                {
+                    int rows = 0;
+
+                    //Count number of rows in csv file
+                    using (var reader = File.OpenText(openLocation))
+                    {
+                        while (reader.ReadLine() != null)
+                        {
+                            rows++;
+                        }
+                    }
+
+                    //creates new array using the amount of rows
+                    data = new string[rows];
+
+                    //Gets megabyte of file
+                    FileInfo csvFile = new FileInfo(openLocation);
+                    long megabyte = csvFile.Length /(1024*1024);
+                    //declares 100mb in bytes as max size
+                    long maxbytes = 100000000;
+
+                    //Populate array with CSV rows
+                    int arrayInjection = 0;
+                    using (var reader = File.OpenText(openLocation))
+                    {
+                        while (reader.ReadLine() != null && arrayInjection != rows)
+                        {
+                            data[arrayInjection] = reader.ReadLine();
+                            arrayInjection++;
+                        }
+                    }
+
+
+                    if (!File.Exists(saveLocation))
+                    {
+                         File.Create(saveLocation).Close();
+                    }
+
+                    //Starts process for saving the file into a new document
+                    StringBuilder sb = new StringBuilder();
+                    for (int row = 0; row < rows; row++)
+                    {
+                        if (sb.Length > maxbytes)
+                        {
+                            break;
+                        }
+                        sb.Append(data[row]);
+                        sb.AppendLine();
+                    }
+
+                    File.WriteAllText(saveLocation, sb.ToString());
+                    textBox3.Text = "Completed";
+                }
+            }
+        }
+    }
+}
